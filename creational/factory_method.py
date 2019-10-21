79eab83dc363d6e5444a295cdab2d81889784246
Gustavo Boaz <gustavo.boaz@hotmail.com>
2019-10-21 15:14:56 -0300

    create factory method

---
+++ creational/factory_method.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+#===========================================Definição de classes abstratas
+class Product(ABC):
+    """ This class is used for implements a new product in the system """
+    @abstractmethod
+    def build_product(self) -> str:
+        """ return the str building """
+        pass
+
+class Creator(ABC):
+    """ This class is used for create of creators of products """
+    @abstractmethod
+    def create_product(self) -> Product:
+        """ return the Product for building """
+        pass
+
+    def build(self) -> str:
+        """ return the content build """
+        _produto = self.create_product()
+        return _produto.build_product()
+#=========================================Definição dos Produtos concretos
+class Product1(Product):
+
+    def build_product(self) -> str:
+        return "Product 1 Build"
+
+class Product2(Product):
+
+    def build_product(self) -> str:
+        return "Product 2 Build"
+#========================================Definição dos Creadores concretos
+class CreatorProduct1(Creator):
+
+    def create_product(self) -> Product1:
+        return Product1()
+
+class CreatorProduct2(Creator):
+
+    def create_product(self) -> Product2:
+        return Product2()
+#======================================================Definição do Cliente
+def fm_client(creator: Creator) -> None:
+    print(creator.build())
+
+def main_fm():
+    while True:
+        try:
+            option = int(input("Criador option [1][2] | Exit[0]: "))
+            if(option == 1):
+                fm_client(CreatorProduct1())
+            elif(option == 2):
+                fm_client(CreatorProduct2())
+            elif(option == 0):
+                break
+        except:
+            print("Option false")
+            continue
+    
+
+
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from creational.factory_method import main_fm
+
+def main():
+    main_fm()
+
+if __name__ == '__main__':
+    main()
