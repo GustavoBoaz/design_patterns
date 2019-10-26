ee626fe4e4a27d15ab43e571973ac0b1104c42bc
Gustavo Boaz <gustavo.boaz@hotmail.com>
2019-10-26 16:31:16 -0300

    comit Singleton

---
+++ creational/singleton.py
@@ -0,0 +1,66 @@
+#===========================================Definição de classes Singleton
+class Singleton(object):
+    _instance = None
+
+    def __new__(self):
+        if (not self._instance):
+            self._instance = super(Singleton, self).__new__(self)
+            self._var = 10
+        return self._instance
+
+#==================Definição de metodo Singleton como Decorator de Classes
+def singleton(myClass):
+    instances = {}
+    def get_instance(*args, **kwargs):
+        if(myClass not in instances):
+            instances[myClass] = myClass(*args, **kwargs)
+        return instances[myClass]
+    return get_instance
+
+@singleton #decorador ativado
+class ProductSingleton(object):
+    
+    def __init__(self, value = None):
+        self._value = value
+    
+    @property
+    def iten(self) -> None:
+        print("Getting: {}".format(self._value))
+        return self._value
+
+    @iten.setter
+    def iten(self, value) -> None:
+        print("Setting: {}".format(value))
+        self._value = value
+
+    @iten.deleter
+    def iten(self):
+        print("Deleting: {}".format(self._value))
+        del self._value
+#=====================================================Definição do Cliente
+def main_s():
+    while True:
+        try:
+            option = int(input("Criar Singleton: Apartir de Classe [1] | Apartir de Metodo [2]  | Exit[0]: "))
+            if(option == 1):
+                instancia0 = Singleton()
+                print("Instancia0 com Valor: {}".format(str(instancia0._var)))
+                instancia0._var = int(input("Alterar Valor do iten: "))
+                print("Valor alterado para: {}".format(str(instancia0._var)))
+                instancia1 = Singleton()
+                print("\nCriado nova instancia de Singleton")
+                print("Instancia1 com Valor: {}".format(str(instancia1._var)))     
+                continue
+            elif(option == 2):
+                instancia0 = ProductSingleton()
+                instancia0.iten
+                instancia0.iten = int(input("Alterar Valor do iten: "))
+                print("\nCriado nova instancia de Singleton")
+                instancia1 = ProductSingleton()
+                instancia1.iten
+                continue
+            elif(option == 0):
+                break
+        except:
+            print("Option false")
+            continue
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from creational.factory_method import main_fm
 from creational.abstract_factory import main_af
 from creational.builder import main_b
 from creational.prodotype import main_p
+from creational.singleton import main_s
 
 def main():
     #main_fm()
     #main_af()
     #main_b()
     #main_p()
+    #main_s()
 
 if __name__ == '__main__':
     main()
